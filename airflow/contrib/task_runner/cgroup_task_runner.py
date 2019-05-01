b293c90495324ae8f4cc2d8d88de011a1be341cd
Junda Yang <youngyjd@gmail.com>
2019-05-01 16:05:53 -0700

    call super.finish to remove temp cfg file

--- airflow/contrib/task_runner/cgroup_task_runner.py
+++ airflow/contrib/task_runner/cgroup_task_runner.py
@@ -193,6 +193,7 @@ class CgroupTaskRunner(BaseTaskRunner):
             self._delete_cgroup(self.mem_cgroup_name)
         if self._created_cpu_cgroup:
             self._delete_cgroup(self.cpu_cgroup_name)
+        super().on_finish()
 
     @staticmethod
     def _get_cgroup_names():
